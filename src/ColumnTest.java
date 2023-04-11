c69b36af806c99a0a0ddb31497a6121fa9a6cec1
Melih Ekizce <darkblue1526@gmail.com>
2023-04-11 14:38:36 +0300

    first commit

---
+++ src/ColumnTest.java
@@ -0,0 +1,12 @@
+
+import java.io.IOException;
+
+public class ColumnTest {
+
+	public static void main(String[] args) throws Exception{
+		AdditionalThings.LoginScreen();
+		Column game = new Column();
+		game.menu();
+	}
+
+}
